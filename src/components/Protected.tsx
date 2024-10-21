f22f5e1a3b80236f338ae76fcbb7ce010a741a8c
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-10-21 22:56:25 +0530

    optimised-protected-routes

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -11,26 +11,22 @@ export default function Protected({
   children,
   authentication = true,
 }: ProtectedProps) {
-  const navigate = useNavigate();
-  const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state: any) => state.auth.status);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (authStatus !== undefined) {
-      if (authStatus) {
-        if (!authentication) {
-          navigate("/");
-        }
-      } else {
-        if (authentication) {
-          navigate("/login");
-        }
-      }
-      setLoader(false);
+    if (authStatus === undefined) {
+      return;
+    }
+
+    if (authStatus && !authentication) {
+      navigate("/");
+    } else if (!authStatus && authentication) {
+      navigate("/login");
     }
   }, [authStatus, navigate, authentication]);
 
-  if (loader) {
+  if (authStatus === undefined) {
     return <h1>Loading...</h1>;
   }
 
