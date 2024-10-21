11a100a2e8c631ca5a70cb147475956a1eca14b9
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-10-21 22:59:56 +0530

    bug-fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,34 @@
 import { Dumbbell, House, UserRound } from "lucide-react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
 
 const Footer = () => {
+  const authstatus = useSelector((state: any) => state.auth.status);
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-zinc-900 shadow-lg">
-      <div className="flex justify-around p-4">
-        <Link to="/" className="flex flex-col items-center text-white">
-          <i className="fas fa-home text-2xl"></i>
-          <span className="text-xs">
-            <House />
-          </span>
-        </Link>
-        <Link to="/workout" className="flex flex-col items-center text-white">
-          <i className="fas fa-dumbbell text-2xl"></i>
-          <span className="text-xs">
-            <Dumbbell />
-          </span>
-        </Link>
-        <Link to="/profile" className="flex flex-col items-center text-white">
-          <i className="fas fa-user text-2xl"></i>
-          <span className="text-xs">
-            <UserRound />
-          </span>
-        </Link>
+    authstatus && (
+      <div className="fixed bottom-0 left-0 right-0 bg-zinc-900 shadow-lg">
+        <div className="flex justify-around p-4">
+          <Link to="/" className="flex flex-col items-center text-white">
+            <i className="fas fa-home text-2xl"></i>
+            <span className="text-xs">
+              <House />
+            </span>
+          </Link>
+          <Link to="/workout" className="flex flex-col items-center text-white">
+            <i className="fas fa-dumbbell text-2xl"></i>
+            <span className="text-xs">
+              <Dumbbell />
+            </span>
+          </Link>
+          <Link to="/profile" className="flex flex-col items-center text-white">
+            <i className="fas fa-user text-2xl"></i>
+            <span className="text-xs">
+              <UserRound />
+            </span>
+          </Link>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
