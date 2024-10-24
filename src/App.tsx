d4edb26047592caccdd9499face8c61039455173
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-10-24 22:37:13 +0530

    loader-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router";
 import { currentuserapi } from "./Api/Authapi.ts";
 import "./App.css";
 import Footer from "./components/Footer.tsx";
-import SkeletonLoader from "./components/Loader.tsx";
 
 function App() {
   const dispatch = useDispatch();
-  const user = useSelector((state: any) => state.auth.user);
 
   useEffect(() => {
     currentuserapi(dispatch);
   }, [dispatch]);
   return (
     <div className=" ">
-      <main>{user ? <Outlet /> : <SkeletonLoader />}</main>
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
